08584b854070aa6e5048f9164724ee4f51fd2f79
rahulraparia-ac <rahulraparia.cs@gmail.com>
2023-07-06 01:06:30 +0530

    Return greetings for multiple people

--- godevdotcom/hello/hello.go
+++ godevdotcom/hello/hello.go
@@ -11,12 +11,16 @@ func main() {
 	// Set properties of the predefined logger, including the log entry prefix and a flag to disable printing the time, source file, and the line number.
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
-	// Request a greeting message.
-	message, err := greetings.Hello("ravikanth")
+
+	// A slice of names."
+	names := []string{"Cici", "Fifi", "Hector", "Pipi"}
+
+	// Request a greeting for each name in the names slice.
+	messages, err := greetings.Hellos(names)
 	// If an error was returned, printing it to the console and esit the program
 	if err != nil {
 		log.Fatal(err)
 	}
-	// If no error was returned, print the returned message to the console
-	fmt.Println(message)
+	// If no error was returned, print the returned map of messages to the console
+	fmt.Println(messages)
 }
