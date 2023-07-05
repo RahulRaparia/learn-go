a1c8cf2ebad4fd52c6d977886be476221b9f8716
rahulraparia-ac <rahulraparia.cs@gmail.com>
2023-07-05 19:18:54 +0530

    error handing for greeting package and hello package

--- godevdotcom/hello/hello.go
+++ godevdotcom/hello/hello.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
-    // Get a greeting message and print it.
-    message := greetings.Hello("Gladys")
-    fmt.Println(message)
+	// Set properties of the predefined logger, including the log entry prefix and a flag to disable printing the time, source file, and the line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+	// Request a greeting message.
+	message, err := greetings.Hello("")
+	// If an error was returned, printing it to the console and esit the program
+	if err != nil {
+		log.Fatal(err)
+	}
+	// If no error was returned, print the returned message to the console
+	fmt.Println(message)
 }
